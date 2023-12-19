2da3ad6763c751c5e51417747b00ea245b28f950
John Carlo Lipa <johncarlolipa@gmail.com>
2023-12-19 08:58:37 +0800

    done exercise fourteen

--- problems.js
+++ problems.js
@@ -145,24 +145,36 @@ function isSubString(searchString, subString) {
 isSubString("k", "Kim");
 
 function aCounter(word) {
-  /* Takes in a word and returns the number of a's within that word. Counts
-    both lowercase (a) and uppercase (A). Your job is to translate the following
-    function to use a `for` loop instead of the `while` loop it is currently
-    using. */
-  /*
-    let index = 0;
+    /* Takes in a word and returns the number of a's within that word. Counts
+      both lowercase (a) and uppercase (A). Your job is to translate the following
+      function to use a `for` loop instead of the `while` loop it is currently
+      using. */
+    /*
+      let index = 0;
+      let count = 0;
+      while (index < word.length) {
+          let char = word[index];
+          if (char === "a" || char === "A") {
+          count += 1;
+          }
+          index++;
+      }
+      return count;
+      */
+
+    // Your code here
     let count = 0;
-    while (index < word.length) {
-        let char = word[index];
-        if (char === "a" || char === "A") {
+    for (let i = 0; i < word.length; i++) {
+      let char = word[i];
+      if (char === "a" || char === "A") {
         count += 1;
-        }
-        index++;
+      }
     }
     return count;
-    */
-  // Your code here
-}
+  }
+
+  console.log(aCounter("Apple"));
+
 
 module.exports = {
   divideByThree,
